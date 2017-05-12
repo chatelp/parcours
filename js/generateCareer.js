e3028f306e6f36114fc4913d308faf41e7a4924f
Pierre CHATEL <chatelp@gmail.com>
2017-05-12 12:09:26 +0200

    Largeur dynamique du diagramme.

--- js/generateCareer.js
+++ js/generateCareer.js
@@ -1,63 +1,62 @@
 //based on http://bl.ocks.org/d3noob/8329447
 
-// ************** Generate the tree diagram	 *****************
-var margin = {
-		top: 20,
-		right: 120,
-		bottom: 20,
-		left: 120
-	},
-	width = 960 - margin.right - margin.left,
-	height = 500 - margin.top - margin.bottom;
+$(document).ready(function () {
+	var margin = {
+			top: 60,
+			right: 120,
+			bottom: 60,
+			left: 120
+		},
+		width = $("#diag").width() - margin.right - margin.left,
+		height = 500 - margin.top - margin.bottom;
 
-var i = 0;
-
-var tree = d3.layout.tree()
-	.size([height, width]);
-
-var diagonal = d3.svg.diagonal()
-	.projection(function (d) {
-		return [d.y, d.x];
+	// load the external data
+	d3.json("treeData.json", function (error, data) {
+		if (data.hasOwnProperty("career"))
+			updateCareer(data.career);
+		if (data.hasOwnProperty("tree"))
+			updateTree(data.tree, margin, width, height);
 	});
-
-var svg = d3.select("#diag").append("svg")
-	.attr("width", width + margin.right + margin.left)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-svg.append("svg:defs").append("svg:marker")
-    .attr("id", "triangle")
-    .attr("refX", 18)
-    .attr("refY", 6)
-    .attr("markerWidth", 30)
-    .attr("markerHeight", 30)
-    .attr("orient", "auto")
-    .append("path")
-    .attr("d", "M 0 0 12 6 0 12 3 6")
-    .style("fill", "black");
-	
-// load the external data
-d3.json("treeData.json", function (error, data) {
-	if(data.hasOwnProperty("career"))
-		updateCareer(data.career);
-	if(data.hasOwnProperty("tree"))
-		updateTree(data.tree);
 });
 
+//Update general career data
 function updateCareer(source) {
-	$(document).ready(function () {
-		document.title = source.careerName;
-		$("h1.header").text(source.careerName);
-		$("h5.header").text(source.careerDescription);
-	});
+	document.title = source.careerName;
+	$("h1.header").text(source.careerName);
+	$("h5.header").text(source.careerDescription);
 }
 
-function updateTree(source) {
+//Update visual career tree
+function updateTree(source, margin, width, height) {
+	var d3Tree = d3.layout.tree()
+		.size([height, width]);
+
+	var diagonal = d3.svg.diagonal()
+		.projection(function (d) {
+			return [d.y, d.x];
+		});
+
+	var svg = d3.select("#diag").append("svg")
+		.attr("width", width + margin.right + margin.left)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	svg.append("svg:defs").append("svg:marker")
+		.attr("id", "triangle")
+		.attr("refX", 18)
+		.attr("refY", 6)
+		.attr("markerWidth", 30)
+		.attr("markerHeight", 30)
+		.attr("orient", "auto")
+		.append("path")
+		.attr("d", "M 0 0 12 6 0 12 3 6")
+		.style("fill", "black");
+	var i = 0;
 
 	// Compute the new tree layout.
-	var nodes = tree.nodes(source).reverse(),
-		links = tree.links(nodes);
+	var nodes = d3Tree.nodes(source).reverse(),
+		links = d3Tree.links(nodes);
 
 	// Normalize for fixed-depth.
 	nodes.forEach(function (d) {
@@ -80,22 +79,22 @@ function updateTree(source) {
 
 	nodeEnter.append("circle")
 		.attr("r", 10)
-		.style("stroke", function (d) {			
+		.style("stroke", function (d) {
 			return d.selected ? "red" : (d.rome ? "green" : "steelblue");
 		});
-		//.append("title")
-   		//.text(function(d) { return d.name; });
-    
-	$('svg circle').tipsy({ 
-        gravity: 'w', 
-        html: true, 
-        title: function() {
-		  var d = this.__data__;
-          var text = d.description ? d.description : d.name;
-		  text = text.concat(' - <span style="color: steelblue">Cliquez pour plus de détails.</span>');
-		  return text; 
-        }
-      });
+	//.append("title")
+	//.text(function(d) { return d.name; });
+
+	$('svg circle').tipsy({
+		gravity: 'w',
+		html: true,
+		title: function () {
+			var d = this.__data__;
+			var text = d.description ? d.description : d.name;
+			text = text.concat(' - <span style="color: steelblue">Cliquez pour plus de détails.</span>');
+			return text;
+		}
+	});
 
 	nodeEnter.append("text")
 		.attr("x", function (d) {
@@ -106,7 +105,7 @@ function updateTree(source) {
 			return d.children || d._children ? "end" : "start";
 		})
 		.text(function (d) {
-			return  d.institution ? d.name.concat(", ", d.institution) : d.name;
+			return d.institution ? d.name.concat(", ", d.institution) : d.name;
 		})
 		.style("fill-opacity", 1);
 
@@ -121,7 +120,7 @@ function updateTree(source) {
 	link.enter().insert("path", "g")
 		.attr("class", "link")
 		.attr("d", diagonal);
-		//.attr("marker-end", "url(#triangle)");
+	//.attr("marker-end", "url(#triangle)");
 
 
 }
